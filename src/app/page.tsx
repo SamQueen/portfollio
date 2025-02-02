e49ce11256075eb4d7d6fc8317bdb29172d7dc7a
Sam Queen <samqueenbusiness@gmail.com>
2025-02-01 23:29:16 -0700

    created animatied text about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,6 +66,7 @@ export default function Home() {
           />
         </div>
 
+
         {(activeSection === 'about') && (
           <AboutSection />
         )}
--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import SectionWrapper from './SectionWrapper';
+import { TypeAnimation } from 'react-type-animation';
 
 const AboutSection = () => {
 
@@ -13,6 +14,25 @@ const AboutSection = () => {
             Hello, my name is <br/>SAM QUEEN
           </h1>
 
+          <div className='mt-10'>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'I create Frontend User Interfaces.',
+                2500, // wait 1s 
+                'I create Web Applications.',
+                2500,
+                'I optimize Web Applications .',
+                2500,
+                'I create unique user experiences.',
+                2500
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: '2em', display: 'inline-block' }}
+              repeat={Infinity}
+            />
+          </div>
 
           <p className='text-lg mt-10'>Welcome to my corner of the web! My name is Sam Queen and I am a passionate developer on a mission to build beautiful web applications. My role as a developer goes beyond writing lines of code; its about turning ideas into functional websites.</p>
         </div>
@@ -21,8 +41,9 @@ const AboutSection = () => {
           <Image 
             className='ml-20'
             src='/meaf2.jpg' 
-            height={300} 
-            width={180} 
+            width={250} 
+            height={0} 
+            layout="intrinsic" 
             alt="profile" 
           />
         </div>
